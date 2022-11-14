8ff57ecefeeee6cae5ee046d7e93749dc69bb517
LizbtehTrujil786 <SpamEm674@gmail.com>
2022-11-14 13:58:16 -0600

    Adding Redirect

--- sspeedsters/src/App.js
+++ sspeedsters/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import pic from "./Web_Images/SpeedstersLogo6.png";
+import { Container, Row } from "react-bootstrap";
+import { HomeScreen } from "./Pages/HomeScreen";
+import { Navigation } from "./Components/Navigation";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+  
+      </Row>
+  
+      <Row>
+        <div className="App">
+          <header className="App-header">
+            <img src={pic} className="App-logo" alt="logo" />
+            <p>Speedsters</p>
+          </header>
+        </div>
+      </Row>
+    </Container>
   );
 }
 
